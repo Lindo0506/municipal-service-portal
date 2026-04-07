63469baecc79afddcdb17d8278e6df0a1c57d308
Lindo0506 <2819280@students.wits.ac.za>
2026-04-07 22:49:55 +0200

    chore : initial project structure CI/CD setupp

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 
 const app = express();
 
+const requests = require('./serviceRequests');
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -10,6 +12,34 @@ app.use(express.json());
 // Test route
 app.get('/', (req, res) => {
   res.send('API is running 🚀');
+});
+
+//(POST ROUTE)
+app.post('/service-requests', (req, res) => {
+  const { category, description, lat, lng } = req.body;
+
+  if (!category || !description) {
+    return res.status(400).json({ error: "Missing fields" });
+  }
+
+  const newRequest = {
+    id: requests.length + 1,
+    category,
+    description,
+    lat,
+    lng,
+    status: "Submitted",
+    createdAt: new Date()
+  };
+
+  requests.push(newRequest);
+
+  res.status(201).json(newRequest);
+});
+
+// ✅ GET ROUTE
+app.get('/service-requests', (req, res) => {
+  res.json(requests);
 });
 
 // Start server
---
+++ backend/serviceRequests.js
@@ -0,0 +1,3 @@
+let requests = [];
+
+module.exports = requests;
